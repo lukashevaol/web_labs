b4829bdad0662257aeed8ce14fee43c58fe04b9b
lukashevaol <lukashevaol@gmail.com>
2019-04-06 18:27:44 +0300

    latest version

---
+++ lab3/script.js
@@ -0,0 +1,89 @@
+function wrapTextTest(context, text, marginLeft, maxWidth, lineHeight) {
+  var words = text.split(" ");
+  var countWords = words.length;
+  var line = "";
+  var linesArr = [];
+  for (var n = 0; n < countWords; n++) {
+    var testLine = line + words[n] + " ";
+    var testWidth = context.measureText(testLine).width;
+    if (testWidth > maxWidth) {
+      linesArr.push(line);
+      line = words[n] + " ";
+    } 
+    else {
+      line = testLine;
+    }
+  }
+  linesArr.push(line);
+  countTextHeight = (linesArr.length - 2) * lineHeight;
+  var marginTop = marginLeft - countTextHeight / 2;
+  for (var m = 0; m < linesArr.length; m++) {
+    context.fillText(linesArr[m], marginLeft, marginTop);
+    marginTop = marginTop + lineHeight;
+  }
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$.ajax({
+  url: "https://api.forismatic.com/api/1.0/",
+  jsonp: "jsonp",
+  dataType: "jsonp",
+  data: {
+    method: "getQuote",
+    lang: "ru",
+    format: "jsonp"
+  }
+})
+.done(doNext);
+
+function doNext(response) {
+
+  text = response.quoteText;
+
+  var canvas = document.createElement('canvas');
+  canvas.width = 400;
+  canvas.height = 400;
+  document.body.appendChild(canvas);
+
+  ctx = canvas.getContext('2d');
+
+  pic1 = new Image();
+  pic2 = new Image();
+  pic3 = new Image();
+  pic4 = new Image();
+
+  var maxTextWidth = 300;
+  var lineHeightText = 20;
+  var marginLeftText = 200;
+
+  x = getRandomInt(180, 220);
+  y = getRandomInt(180, 220);
+
+  pic1.src = 'https://source.unsplash.com/collection/1127163/330x220';
+  pic1.onload = function() { 
+    ctx.drawImage(pic1, 0, 0);
+    pic2.src = 'https://source.unsplash.com/collection/1127160/330x220';
+    pic2.onload = function() {
+      ctx.drawImage(pic2, x, 0);
+      pic3.src = 'https://source.unsplash.com/collection/1127155/330x220';
+      pic3.onload = function() {
+        ctx.drawImage(pic3, 0, y);
+        pic4.src = 'https://source.unsplash.com/collection/1127353/330x220';
+        pic4.onload = function() {
+          ctx.drawImage(pic4, x, y);
+          ctx.rect(0, 0, canvas.width, canvas.height);
+          ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
+          ctx.fill();
+          ctx.textAlign = "center";
+          textBaseline = "middle";
+          ctx.font = "18px 'Montserrat', Helvetica, Verdana, sans-serif";
+          ctx.fillStyle = "white";
+          wrapTextTest(ctx, text, marginLeftText, maxTextWidth, lineHeightText);    
+        }
+      }
+    }
+  }
+}
