19b07727fa014bae1b14df31d392e19e5129a217
lukashevaol <lukashevaol@gmail.com>
2019-04-20 19:13:06 +0300

    made input fields required and created error messages

--- lab4/form1.php
+++ lab4/form1.php
@@ -1,48 +1,71 @@
 <!DOCTYPE HTML>
-<html>  
-<body>
+  <html>  
+    <body>
 
-<?php
-$name = $email = $gender = $comment = $website = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $headline = test_input($_POST["headline"]);
-  $story = test_input($_POST["story"]);
-}
+    <?php
+      $nameErr = $emailErr = $headlineErr = $storyErr = "";
+      $name = $email = $headline = $story = "";
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+          } else {
+            $name = test_input($_POST["name"]);
+          }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+          if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+          } else {
+            $email = test_input($_POST["email"]);
+          }
 
-?>
+          if (empty($_POST["headline"])) {
+            $headlineErr = "Headline is required";
+          } else {
+            $headline = test_input($_POST["headline"]);
+          }
 
+          if (empty($_POST["story"])) {
+            $storyErr = "Story is required";
+          } else {
+            $story = test_input($_POST["story"]);
+          }          
+      }
 
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name">
-  <br><br>
-  E-mail: <input type="text" name="email">
-  <br><br>
-  Headline: <input type="text" name="headline">
-  <br><br>
-  News Story: <textarea name="story" rows="5" cols="40"></textarea>
-  <br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
+      function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 
-<?php
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $headline;
-echo "<br>";
-echo $story;
-?>
+    ?>
 
 
-</body>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+      Name: <input type="text" name="name">
+      <span class="error">* <?php echo $nameErr;?></span>
+      <br><br>
+      E-mail: <input type="text" name="email">
+      <span class="error">* <?php echo $emailErr;?></span>
+      <br><br>
+      Headline: <input type="text" name="headline">
+      <span class="error">* <?php echo $headlineErr;?></span>
+      <br><br>
+      News Story: <textarea name="story" rows="5" cols="40"></textarea>
+      <span class="error">* <?php echo $storyErr;?></span>
+      <br><br>
+      <input type="submit" name="submit" value="Submit">  
+    </form>
+
+    <?php
+      echo $name;
+      echo "<br>";
+      echo $email;
+      echo "<br>";
+      echo $headline;
+      echo "<br>";
+      echo $story;
+    ?>
+
+  </body>
 </html>
