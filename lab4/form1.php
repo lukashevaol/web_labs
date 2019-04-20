6fe9b474483a90463ecd33eaf05fb0332780df67
lukashevaol <lukashevaol@gmail.com>
2019-04-20 23:11:44 +0300

    inserts data in table 'news'

--- lab4/form1.php
+++ lab4/form1.php
@@ -66,15 +66,41 @@
 
 
     <?php
-			if( isset( $_POST['submit'] ) )
+
+    ini_set('display_errors',1);
+		error_reporting(E_ALL);
+
+			if( isset( $_POST['submit'] ) and $name != "" and $email != "" and $headline != "" and $story != "")
       {
-    		echo $name;
-      	echo "<br>";
-      	echo $email;
-      	echo "<br>";
-      	echo $headline;
-      	echo "<br>";
-      	echo $story;
+      	$user = 'root';
+				$password = 'root';
+				$db = 'news_db';
+				$host = 'localhost';
+				$port = 3306;
+
+				$link = mysqli_init();
+				$conn = mysqli_real_connect(
+   				$link, 
+   				$host, 
+   				$user, 
+   				$password, 
+   				$db,
+   				$port
+				);
+
+				if (!$link) {
+    			die("Connection failed: " . mysqli_connect_error());
+				}
+				echo "Connected successfully";
+				$sql = ("INSERT INTO news (name, email, headline, story, timestamp) VALUES ('$name', '$email', '$headline', '$story', NOW())");
+
+				if (mysqli_query($link, $sql)) {
+    			echo "New record created successfully";
+				} else {
+    			echo "Error: " . mysqli_error($link);
+				}
+
+				mysqli_close($link);
     	}
     ?>
 
