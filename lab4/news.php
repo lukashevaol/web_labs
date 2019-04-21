0a4fd0f11ab5e14e3a96238145ef222f44a32131
lukashevaol <lukashevaol@gmail.com>
2019-04-21 22:37:59 +0300

    added css styles; made admin

--- lab4/news.php
+++ lab4/news.php
@@ -1,7 +1,21 @@
 <!DOCTYPE HTML>
 <html>
+
+
 <head>
-</head> 
+  <meta charset="utf-8">
+  <title>News Portal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<div class="header">
+    <a href="news.php">News Portal</a>
+    <div class="menu">
+      <a href="form1.php">Add news</a>
+      <a href="news.php?rights=admin">Admin</a>
+    </div>
+</div>
+ 
 
 <body>
 
@@ -9,6 +23,7 @@
 
   ini_set('display_errors',1);
   error_reporting(E_ALL);
+  $rights = "";
 
   $user = 'root';
   $password = 'root';
@@ -28,8 +43,7 @@
 
   if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
-  }
-  echo "Connected successfully <br>";
+  } 
 
   $sql = ("SELECT id, name, email, headline, story, timestamp FROM news");
 
@@ -38,22 +52,36 @@
       while($row = mysqli_fetch_assoc($result)) {
 
     ?>
-   <font><b><? echo $row["headline"]; ?></b> <i><? echo $row["timestamp"]; ?></i></font>
-   <br>
-   <font size="-1"><a href="news.php?a=delete&id=<? echo $row["id"]; ?>">delete</a></font>
-   <br>
+
+    <div class="news_block">
+      <div class="news_headline"> 
+        <? echo $row["headline"]; ?>
+      </div>
+
+         <i><? echo $row["email"], " ", $row["timestamp"]; ?></i>
+      <p><? echo $row["story"]; ?></p>
+
+      <? if (isset($_GET["rights"])) {?>
+      <div class="delete">
+        <a href="news.php?a=delete&id=<? echo $row["id"]; ?>">delete</a>
+      </div>
+    <? } ?>
+
+    </div>
+
     <?php
       }
     } else {
-      ?>
-      <font size="-1">No news in the database</font>
-      <?php
-    }
+    ?>
+
+    <p>No news in the database</p>
     
-  } else {
-    echo "Error: " . $sql . mysqli_error($link);
-  }
-  mysqli_close($link);
+    <?php
+    }
+    } else {
+      echo "Error: " . $sql . mysqli_error($link);
+    }
+    mysqli_close($link);
 
 if (isset($_GET["a"])) {
   if ($_GET["a"] == "delete"){
@@ -78,18 +106,17 @@ if (isset($_GET["a"])) {
   if (!$link) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  echo "Connected successfully <br>";
   $query = "DELETE FROM news WHERE id=$id";
 
   if (mysqli_query($link, $query)) {
+    header("Refresh: 0; url=news.php?rights=admin");
     echo "Record deleted successfully";
   } else {
+    header("Refresh: 0; url=news.php?rights=admin");
     echo "Error deleting record: " . mysqli_error($link);
   }
 
   mysqli_close($link);
-
-
   }
 }
 
