110396f3f94f464182a73caec44ec154103ead5e
lukashevaol <lukashevaol@gmail.com>
2019-05-05 10:58:29 +0300

    added server

--- lab5/md-editor/src/main.js
+++ lab5/md-editor/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import 'D:\\универ\\web2019\\web_labs\\lab5\\md-editor\\src\\plugins\\bootstrap'
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
